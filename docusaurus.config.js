fe16644f55d07ae1311ba6e6ada79d5680377303
WriteCodeJG <jgillespie.us@gmail.com>
2025-08-02 15:31:27 -0500

    Add trailingSlash config setting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'j-gillespie', // Usually your GitHub org/user name.
   projectName: 'portfolio', // Usually your repo name.
-
+  trailingSlah: true,
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
