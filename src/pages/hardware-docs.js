b6b5c40a4f05350913ffb4c25c1f8e7161f77331
WriteCodeJG <jgillespie.us@gmail.com>
2025-08-10 15:11:50 -0500

    Add sbRIO-9687 sample, and demote H2s to paragraphs on sample description pages.

--- src/pages/hardware-docs.js
+++ src/pages/hardware-docs.js
@@ -8,8 +8,11 @@ return (
 <div style={{ padding: '2rem' }}>
 <h1>Hardware Documentation</h1>
 
-<h2>Hardware User Manual</h2>
-<p>The <a href="pixe-8521_um.pdf" target="_blank" rel="noopener noreferrer">PXIe-8521 User Manual</a> provides information for NI’s first Automotive Ethernet PXI Express module. The manual covers installation, configuration, and basic operation, guiding the user from unpacking the kit through connecting and preparing the device for use. I authored the manual using Oxygen Author to develop structured DITA content and Adobe Illustrator to create SVG illustrations. I delivered a PDF and print specifications to the print shop for printed copies to be included in the hardware kits, and I prepared the PDF for publication on the NI website, integrating appropriate product codes and metadata.</p>
+<p><em>PXIe-8521 User Manual</em></p>
+<p>This <a href="pixe-8521_um.pdf" target="_blank" rel="noopener noreferrer">hardware user manual</a> provides information for the PXIe-8521, NI’s first Automotive Ethernet PXI Express module. The manual covers installation, configuration, and basic operation, guiding the user from unpacking the kit through connecting and preparing the device for use. I authored the manual using Oxygen Author to develop structured DITA content and Adobe Illustrator to create SVG illustrations. I delivered a PDF and print specifications to the print shop for printed copies to be included in the hardware kits, and I prepared the PDF for publication on the NI website, integrating appropriate product codes and metadata.</p>
+
+<p><em>sbRIO-9687 Specifications</em></p>
+<p>The sbRIO-9687 is a universal interface board designed to be stacked with an sbRIO general purpose inverter controller (GPIC) to enable direct cable connectivity to Semikron power inverters. I collaborated with with the lead engineer and our safety/compliance team to ensure the <a href="sbrio-9687_specs.pdf" target="_blank" rel="noopener noreferrer">hardware specifications</a> were technically accurate and met all relevant regulatory standards. The content combines reused DITA topics from the CMS with new material created using Oxygen Author. The final deliverable was the PDF, which was published to the ni.com website. The document supported marketing and sales initiatives and served as a key resource for field engineers and system integrators.</p>
 
 </div>
 </Layout>
--- src/pages/user-docs.js
+++ src/pages/user-docs.js
@@ -8,8 +8,8 @@ return (
 <div style={{ padding: '2rem' }}>
 <h1>User Documentation</h1>
 
-<h2>Getting Started Guide (Excerpt)</h2>
-<p>This <a href="vsms-user-sample.pdf" target="_blank" rel="noopener noreferrer">excerpt from the <em>HP TippingPoint vSMS Getting Started Guide v4.1</em></a> provides instructions for configuring the virtual client to log into the HP TippingPoint Security Management System (SMS). As an information developer at HP TippingPoint, I developed documentation to support network security professionals integrating these solutions into their environments. I created documentation for software and hardware, including intrusion prevention system (IPS) devices and the platform’s first next-generation firewall system. I authored content in FrameMaker, used Illustrator and Visio for images and diagrams, and published documents in PDF format for enterprise distribution.</p>
+<p><em>HP TippingPoint vSMS Getting Started Guide v4.1</em></p>
+<p>This <a href="vsms-user-sample.pdf" target="_blank" rel="noopener noreferrer">one-page excerpt</a> from the <em>vSMS Getting Started Guide</em> provides instructions for configuring the virtual client to log into the HP TippingPoint Security Management System (SMS). As an information developer at HP TippingPoint, I developed documentation to support network security professionals integrating these solutions into their environments. I created documentation for software and hardware, including intrusion prevention system (IPS) devices and the platform’s first next-generation firewall system. I authored content in FrameMaker, used Illustrator and Visio for images and diagrams, and published documents in PDF format for enterprise distribution.</p>
 
 </div>
 </Layout>
